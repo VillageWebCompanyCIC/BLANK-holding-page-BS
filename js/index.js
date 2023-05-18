f1c783aede1617b035b04813f69d7dd12c4f0f00
Zhong Yi Yan <83918638+LucaYan0506@users.noreply.github.com>
2023-05-18 10:52:31 +0100

    First commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function show_mobile_nav(){
+    document.querySelector('#mobile-navbar').style.display ="block";
+}
+
+function hide_mobile_nav(){
+    document.querySelector('#mobile-navbar').style.display="";
+}
